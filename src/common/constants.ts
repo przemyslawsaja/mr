686fb6575a35931a3d396bfe4e5a9501e570ff6f
Przemyslaw Saja <przemyslaw@pushpushgo.com>
2022-04-25 19:40:01 +0200

    rm constants

--- src/common/constants.ts
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_LATITUDE = 50;
-export const DEFAULT_LONGITUDE = 20;
-export const DEFAULT_ZOOM = 10;
--- src/common/helpers.ts
+++
@@ -1,5 +0,0 @@
-export function omit(obj: any, params: any[]) {
-  obj = { ...obj };
-  params.forEach(param => delete obj[param]);
-  return obj;
-}
